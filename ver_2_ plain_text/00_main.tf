ac5fa69455132ca196addf4eb1a1b6448c91912c
Steve ChoCho <cyk3233@gmail.com>
2022-08-24 11:22:21 +0900

    ver_2_plain_text

--- ver_2_ plain_text/00_main.tf
+++ ver_2_ plain_text/00_main.tf
@@ -9,22 +9,7 @@ terraform {
       version = "4.22.0"
     }
   }
-  # ++++++++++++++++++++++++++++++++++++++++++++++++++++
-  #           State file upload & lock
-  #  다중 인원 및 원격 위치에서의 State 파일 참조를 위해서 설정
-  # ++++++++++++++++++++++++++++++++++++++++++++++++++++
-  # backend "s3" {
-  #   bucket = "test-terraform-state-backend-yg"
-  #   dynamodb_table = "test-terraform-state-locks"
-  #   key = "main/terraform.tfstate"
-  #   region = "ap-northeast-2"
-  #   encrypt = true
-  # }
 }
-
-
-
-
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Terrafrom Version 설정 참고 내용
 # ++++++++++++++++++++++++++++++++++++++++++++++++++++
