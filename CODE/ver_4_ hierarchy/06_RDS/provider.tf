a03a1d66d57739d68ee1ca20527c4758b62d44e8
YGCHO-repo <41570538+YGCHO-repo@users.noreply.github.com>
2023-08-07 16:00:40 +0900

    Delete CODE/ver_4_ hierarchy directory

--- CODE/ver_4_ hierarchy/06_RDS/provider.tf
+++
@@ -1,7 +0,0 @@
-# ++++++++++++++++++++++++++++++++++++++
-#            Provider 설정 
-# ++++++++++++++++++++++++++++++++++++++
-provider "aws" {
-  # region = "ap-northeast-2"
-  region = var.region
-}
